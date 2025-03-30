44d43cd5295854ba19c5392185b0f51ce370d3b3
Chisanan232 <chi10211201@cycu.org.tw>
2025-03-30 11:44:07 +0800

    ➕ Add more scenarios about running program by Poetry runtime environment.
    Signed-off-by: Chisanan232 <chi10211201@cycu.org.tw>

--- test/unit_test/model/subcmd_common.py
+++ test/unit_test/model/subcmd_common.py
@@ -8,6 +8,7 @@ from fake_api_server.model.subcmd_common import SysArg
 USE_PYTHON_FILE_RUN_CMD: str = "pyfake.py"
 USE_RELATIVE_PYTHON_FILE_PATH_RUN_CMD: str = "./pyfake.py"
 USE_MORE_RELATIVE_PYTHON_FILE_PATH_RUN_CMD: str = "./test/pyfake.py"
+USE_POETRY_RUN_CMD: str = "/Users/bryant/Library/Caches/pypoetry/virtualenvs/fake-api-server-LE7TJquz-py3.12/bin/fake"
 
 
 class TestSysArg:
@@ -60,6 +61,33 @@ class TestSysArg:
                         pre_subcmd=SysArg(pre_subcmd=None, subcmd=SubCommandLine.Base), subcmd=SubCommandLine.RestServer
                     ),
                     subcmd=SubCommandLine.Run,
+                ),
+            ),
+            # run sub-command by Poetry environment
+            ([USE_POETRY_RUN_CMD, "--help"], SysArg(pre_subcmd=None, subcmd=SubCommandLine.Base)),
+            (
+                [USE_POETRY_RUN_CMD, "rest-server", "-h"],
+                SysArg(
+                    pre_subcmd=SysArg(pre_subcmd=None, subcmd=SubCommandLine.Base),
+                    subcmd=SubCommandLine.RestServer,
+                ),
+            ),
+            (
+                [USE_POETRY_RUN_CMD, "rest-server", "run", "-h"],
+                SysArg(
+                    pre_subcmd=SysArg(
+                        pre_subcmd=SysArg(pre_subcmd=None, subcmd=SubCommandLine.Base), subcmd=SubCommandLine.RestServer
+                    ),
+                    subcmd=SubCommandLine.Run,
+                ),
+            ),
+            (
+                [USE_POETRY_RUN_CMD, "rest-server", "pull", "-h"],
+                SysArg(
+                    pre_subcmd=SysArg(
+                        pre_subcmd=SysArg(pre_subcmd=None, subcmd=SubCommandLine.Base), subcmd=SubCommandLine.RestServer
+                    ),
+                    subcmd=SubCommandLine.Pull,
                 ),
             ),
         ],
